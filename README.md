2ea1c1d348f6a96e91221184cacc276f7c92bd34
ireaneus <bwdave@protonmail.com>
2022-10-03 11:00:37 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-complete-redux-cart
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-lcgcrv)
---
+++ src/components/Cart/CartButton.jsx
@@ -0,0 +1,20 @@
+import { uiActions } from '../../store/ui-slice';
+import { useDispatch } from 'react-redux';
+import classes from './CartButton.module.css';
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+
+  const toggleHandler = () => {
+    dispatch(uiActions.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>1</span>
+    </button>
+  );
+};
+
+export default CartButton;
