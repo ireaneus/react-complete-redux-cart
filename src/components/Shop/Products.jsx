971076ead660eced920dde3e3e5fd4b042f7ddff
ireaneus <bwdave@protonmail.com>
2022-10-03 16:29:14 -0600

    Redux and Action Create

--- src/components/Shop/Products.jsx
+++ src/components/Shop/Products.jsx
@@ -1,16 +1,36 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'NASB study bible',
+    description: 'An in-depth New American Standard study bible'
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'ESV study bible',
+    description: 'An in-depth English study bible'
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+        {DUMMY_PRODUCTS.map((product) => (
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        key={product.id}
+        id={product.id}
+        title={product.title}
+        price={product.price}
+        description={product.description}
+      />
+        ))}
+
       </ul>
     </section>
   );
